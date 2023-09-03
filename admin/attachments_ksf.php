61e4c42e37619020ce7f8e40ed0f95a0706d0e6d
root@fhsws002.ksfraser.com <root@fhsws002.ksfraser.com>
2023-09-03 14:19:54 -0600

    Mantis 2473 - Attachments not displaying correctly. 	Change from sending file contents to sending a URL to the file itself. 	Worked on new code for self.
    	modified:   attachments.php
    	new file:   attachments_ksf.php

--- admin/attachments_ksf.php
+++ admin/attachments_ksf.php
@@ -28,30 +28,19 @@ else
 if ($view_id != -1)
 {
 	$row = get_attachment($view_id);
-/**	20230903
-	Mantis 2473
-	The attachment is not being correctly displayed.
-*/
 	if ($row['filename'] != "")
 	{
 		if(in_ajax()) {
 			$Ajax->popup($_SERVER['PHP_SELF'].'?vw='.$view_id);
 		} else {
 			$type = ($row['filetype']) ? $row['filetype'] : 'application/octet-stream';	
-/*
     		header("Content-type: ".$type);
     		header('Content-Length: '.$row['filesize']);
-	    	if ($type == 'application/octet-stream')
-    			header('Content-Disposition: attachment; filename='.$row['filename']);
-    		else
+	    	//if ($type == 'application/octet-stream')
+    		//	header('Content-Disposition: attachment; filename='.$row['filename']);
+    		//else
 	 			header("Content-Disposition: inline");
 	    	echo file_get_contents(company_path(). "/attachments/".$row['unique_name']);
-*/
-/**Mantis 2473
-	Pop open a new tab with the direct link ot the content rather than the misdirection above!
-*/
-		header( "location: " . $_SERVER['PHP_SELF'] . "/../" . company_path(). "/attachments/".$row['unique_name'] );
-/** ! Mantis 2473 */
     		exit();
 		}
 	}	
@@ -85,6 +74,79 @@ if ($use_popup_windows)
 page(_($help_context = "Attach Documents"), false, false, "", $js);
 
 simple_page_mode(true);
+
+/***//************************************
+* Make sure the Attachments directory exists.  Create if not.
+*
+* @returns string path of directory
+****************************************/
+function check_make_attachment_dir()
+{
+	$dir =  company_path()."/attachments";
+	if (!file_exists($dir))
+	{
+		mkdir ($dir,0777);
+		$index_file = "<?php\nheader(\"Location: ../index.php\");\n?>";
+		$fp = fopen($dir."/index.php", "w");
+		fwrite($fp, $index_file);
+		fclose($fp);
+	}
+	return $dir;
+}
+
+class ksf_file_upload_attachment extends ksf_file_upload
+{
+/***INHERITED
+	protected $fp;  //!< @var handle File Pointer
+        protected $filename;    //!< @var string name of output file
+        protected $tmp_dir;     //!< @var string temporary directory name
+        protected $path;        //!<DIR where are the images stored.  default company/X/images...
+        protected $filesize;    //!<int
+        protected $filepath;    //!<string full path
+        protected $filecontents;        //!<binary file contents.
+	protected $filetype;	//!< MIME type
+        protected $upload_ok;
+        protected $files_array;         //!< array List of filenames of files we uploaded
+        protected $filepaths_array;     //!< array List of full path filenames of files we uploaded
+        protected $ui_class;            //!< class that has the UI screens required for this class to work
+        protected $upload_button_name;
+        protected $upload_button_label;
+        protected $upload_file_field_name;
+        protected $b_upload_single_file;
+        protected $a_data;              //!< array data returned from file type handler
+***/
+	protected $tmpname;	//!<string temp name from uploading
+        /**//************************************************************************
+        * Construct the class that handles a file upload
+        *
+        * @param pointer _FILES from server
+        * @returns NONE
+        ****************************************************************************/
+        function __construct( $file_global )
+	{
+		//$content = base64_encode(file_get_contents($_FILES['filename']['tmp_name']));
+		$this->set( "tmpname", $_FILES['filename']['tmp_name'] );
+		$filename = basename($_FILES['filename']['name']);
+		parent::__construct( $filename, null, "import_files", true );
+		$this->set( "filesize", $_FILES['filename']['size'] );
+		$this->set( "filetype", $_FILES['filename']['type'] );
+	}
+	/**//********************************
+	* Move the attached file to the attachments directory
+	*
+	* @param string destination directory
+	* @param string filename in destdir
+	*************************************/
+	function move_attachment( $destdir, $unique_name )
+	{
+	
+		move_uploaded_file($tihs->get( "tmpname" ), $destdir."/".$unique_name);
+	}
+
+}
+
+	
+
 //----------------------------------------------------------------------------------------
 if (isset($_GET['filterType'])) // catch up external links
 	$_POST['filterType'] = $_GET['filterType'];
@@ -98,9 +160,8 @@ if ($Mode == 'ADD_ITEM' || $Mode == 'UPDATE_ITEM')
 	elseif ($Mode == 'ADD_ITEM' && (!isset($_FILES['filename']) || $_FILES['filename']['size'] == 0))
 		display_error(_("Select attachment file."));
 	else {
-		//$content = base64_encode(file_get_contents($_FILES['filename']['tmp_name']));
-		$tmpname = $_FILES['filename']['tmp_name'];
-
+		$dir = check_make_attachment_dir();
+/*
 		$dir =  company_path()."/attachments";
 		if (!file_exists($dir))
 		{
@@ -110,10 +171,15 @@ if ($Mode == 'ADD_ITEM' || $Mode == 'UPDATE_ITEM')
 			fwrite($fp, $index_file);
 			fclose($fp);
 		}
-
+*/
+		$upfile = new ksf_file_upload_attachment( $_FILES );
+/*
+		//$content = base64_encode(file_get_contents($_FILES['filename']['tmp_name']));
+		$tmpname = $_FILES['filename']['tmp_name'];
 		$filename = basename($_FILES['filename']['name']);
 		$filesize = $_FILES['filename']['size'];
 		$filetype = $_FILES['filename']['type'];
+*/
 
 		// file name compatible with POSIX
 		// protect against directory traversal
@@ -130,7 +196,10 @@ if ($Mode == 'ADD_ITEM' || $Mode == 'UPDATE_ITEM')
 			$unique_name = uniqid('');
 
 		//save the file
+/*
 		move_uploaded_file($tmpname, $dir."/".$unique_name);
+*/
+		$upfile->move_attachment( $dir, $unique_name )
 
 		if ($Mode == 'ADD_ITEM')
 		{
