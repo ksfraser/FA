3d4e30de7b4e7f57b594763f55bf1a505c318a0c
Kevin Fraser <fraser.ks@gmail.com>
2025-11-18 12:39:19 -0700

    Refactor UI strings in admin/company_preferences.php to use constants for multilingual support

--- admin/company_preferences.php
+++ admin/company_preferences.php
@@ -13,10 +13,11 @@ $page_security = 'SA_SETUPCOMPANY';
 $path_to_root = "..";
 include($path_to_root . "/includes/session.inc");
 
-page(_($help_context = "Company Setup"));
+page(_($help_context = UI_TEXT_COMPANY_SETUP_TITLE));
 
 include_once($path_to_root . "/includes/date_functions.inc");
 include_once($path_to_root . "/includes/ui.inc");
+include_once($path_to_root . "/includes/ui_strings.php");
 
 include_once($path_to_root . "/admin/db/company_db.inc");
 include_once($path_to_root . "/reporting/includes/tcpdf.php");
@@ -28,54 +29,54 @@ if (isset($_POST['update']) && $_POST['update'] != "")
 
 	if (!check_num('login_tout', 10))
 	{
-		UiMessageService::displayError(_("Login timeout must be positive number not less than 10."));
+		UiMessageService::displayError(_(UI_TEXT_LOGIN_TIMEOUT_ERROR));
 		set_focus('login_tout');
 		$input_error = 1;
 	}
 	if (strlen($_POST['coy_name'])==0)
 	{
 		$input_error = 1;
-		UiMessageService::displayError(_("The company name must be entered."));
+		UiMessageService::displayError(_(UI_TEXT_COMPANY_NAME_REQUIRED_ERROR));
 		set_focus('coy_name');
 	}
 	if (!check_num('tax_prd', 1))
 	{
-		UiMessageService::displayError(_("Tax Periods must be positive number."));
+		UiMessageService::displayError(_(UI_TEXT_TAX_PERIODS_ERROR));
 		set_focus('tax_prd');
 		$input_error = 1;
 	}
 	if (!check_num('tax_last', 1))
 	{
-		UiMessageService::displayError(_("Tax Last Periods must be positive number."));
+		UiMessageService::displayError(_(UI_TEXT_TAX_LAST_PERIODS_ERROR));
 		set_focus('tax_last');
 		$input_error = 1;
 	}
 	if (!check_num('round_to', 1))
 	{
-		UiMessageService::displayError(_("Round Calculated field must be a positive number."));
+		UiMessageService::displayError(_(UI_TEXT_ROUND_CALCULATED_ERROR));
 		set_focus('round_to');
 		$input_error = 1;
 	}
 	if (!check_num('max_days_in_docs', 1))
 	{
-		UiMessageService::displayError(_("Max day range in Documents must be a positive number."));
+		UiMessageService::displayError(_(UI_TEXT_MAX_DAY_RANGE_ERROR));
 		set_focus('max_days_in_docs');
 		$input_error = 1;
 	}
 	if ($_POST['add_pct'] != "" && !is_numeric($_POST['add_pct']))
 	{
-		UiMessageService::displayError(_("Add Price from Std Cost field must be number."));
+		UiMessageService::displayError(_(UI_TEXT_ADD_PRICE_FROM_STD_COST_ERROR));
 		set_focus('add_pct');
 		$input_error = 1;
 	}	
 	if (isset($_FILES['pic']) && $_FILES['pic']['name'] != '')
 	{
     if ($_FILES['pic']['error'] == UPLOAD_ERR_INI_SIZE) {
-			UiMessageService::displayError(_('The file size is over the maximum allowed.'));
+			UiMessageService::displayError(_(UI_TEXT_FILE_SIZE_OVER_MAX_ERROR));
 			$input_error = 1;
     }
     elseif ($_FILES['pic']['error'] > 0) {
-			UiMessageService::displayError(_('Error uploading logo file.'));
+			UiMessageService::displayError(_(UI_TEXT_ERROR_UPLOADING_LOGO_FILE));
 			$input_error = 1;
     }
 		$result = $_FILES['pic']['error'];
@@ -160,7 +161,7 @@ if (isset($_POST['update']) && $_POST['update'] != "")
 		);
 
 		$_SESSION['wa_current_user']->timeout = $_POST['login_tout'];
-		display_notification_centered(_("Company setup has been updated."));
+		display_notification_centered(_(UI_TEXT_COMPANY_SETUP_UPDATED_NOTICE));
 		set_focus('coy_name');
 		$Ajax->activate('_page_body');
 	}
@@ -260,74 +261,74 @@ $_POST['use_fixed_assets']  = $myrow["use_fixed_assets"];
 start_outer_table(TABLESTYLE2);
 
 table_section(1);
-table_section_title(_("General settings"));
+table_section_title(_(UI_TEXT_GENERAL_SETTINGS));
 
-text_row_ex(_("Name (to appear on reports):"), 'coy_name', 50, 50);
-textarea_row(_("Address:"), 'postal_address', $_POST['postal_address'], 34, 5);
-text_row_ex(_("Domicile:"), 'domicile', 25, 55);
+text_row_ex(_(UI_TEXT_COMPANY_NAME_LABEL), 'coy_name', 50, 50);
+textarea_row(_(UI_TEXT_ADDRESS_LABEL), 'postal_address', $_POST['postal_address'], 34, 5);
+text_row_ex(_(UI_TEXT_DOMICILE_LABEL), 'domicile', 25, 55);
 
-text_row_ex(_("Phone Number:"), 'phone', 25, 55);
-text_row_ex(_("Fax Number:"), 'fax', 25);
-email_row_ex(_("Email Address:"), 'email', 50, 55);
+text_row_ex(_(UI_TEXT_PHONE_NUMBER_LABEL), 'phone', 25, 55);
+text_row_ex(_(UI_TEXT_FAX_NUMBER_LABEL), 'fax', 25);
+email_row_ex(_(UI_TEXT_EMAIL_ADDRESS_LABEL), 'email', 50, 55);
 
-email_row_ex(_("BCC Address for all outgoing mails:"), 'bcc_email', 50, 55);
+email_row_ex(_(UI_TEXT_BCC_EMAIL_LABEL), 'bcc_email', 50, 55);
 
-text_row_ex(_("Official Company Number:"), 'coy_no', 25);
-text_row_ex(_("GSTNo:"), 'gst_no', 25);
-currencies_list_row(_("Home Currency:"), 'curr_default', $_POST['curr_default']);
+text_row_ex(_(UI_TEXT_OFFICIAL_COMPANY_NUMBER_LABEL), 'coy_no', 25);
+text_row_ex(_(UI_TEXT_GST_NO_LABEL), 'gst_no', 25);
+currencies_list_row(_(UI_TEXT_HOME_CURRENCY_LABEL), 'curr_default', $_POST['curr_default']);
 
-label_row(_("Company Logo:"), $_POST['coy_logo']);
-file_row(_("New Company Logo (.jpg)") . ":", 'pic', 'pic');
-check_row(_("Delete Company Logo:"), 'del_coy_logo', $_POST['del_coy_logo']);
+label_row(_(UI_TEXT_COMPANY_LOGO_LABEL), $_POST['coy_logo']);
+file_row(_(UI_TEXT_NEW_COMPANY_LOGO_LABEL) . ":", 'pic', 'pic');
+check_row(_(UI_TEXT_DELETE_COMPANY_LOGO_LABEL), 'del_coy_logo', $_POST['del_coy_logo']);
 
-check_row(_("Time Zone on Reports"), 'time_zone', $_POST['time_zone']);
-check_row(_("Company Logo on Reports"), 'company_logo_report', $_POST['company_logo_report']);
-check_row(_("Use Barcodes on Stocks"), 'barcodes_on_stock', $_POST['barcodes_on_stock']);
-check_row(_("Auto Increase of Document References"), 'ref_no_auto_increase', $_POST['ref_no_auto_increase']);
-check_row(_("Use Dimensions on Recurrent Invoices"), 'dim_on_recurrent_invoice', $_POST['dim_on_recurrent_invoice']);
-check_row(_("Use Long Descriptions on Invoices"), 'long_description_invoice', $_POST['long_description_invoice']);
-check_row(_("Company Logo on Views"), 'company_logo_on_views', $_POST['company_logo_on_views']);
-label_row(_("Database Scheme Version"), $_POST['version_id']);
+check_row(_(UI_TEXT_TIME_ZONE_ON_REPORTS_LABEL), 'time_zone', $_POST['time_zone']);
+check_row(_(UI_TEXT_COMPANY_LOGO_ON_REPORTS_LABEL), 'company_logo_report', $_POST['company_logo_report']);
+check_row(_(UI_TEXT_USE_BARCODES_ON_STOCKS_LABEL), 'barcodes_on_stock', $_POST['barcodes_on_stock']);
+check_row(_(UI_TEXT_AUTO_INCREASE_DOC_REFS_LABEL), 'ref_no_auto_increase', $_POST['ref_no_auto_increase']);
+check_row(_(UI_TEXT_USE_DIMENSIONS_ON_RECURRENT_INVOICES_LABEL), 'dim_on_recurrent_invoice', $_POST['dim_on_recurrent_invoice']);
+check_row(_(UI_TEXT_USE_LONG_DESCRIPTIONS_ON_INVOICES_LABEL), 'long_description_invoice', $_POST['long_description_invoice']);
+check_row(_(UI_TEXT_COMPANY_LOGO_ON_VIEWS_LABEL), 'company_logo_on_views', $_POST['company_logo_on_views']);
+label_row(_(UI_TEXT_DATABASE_SCHEME_VERSION_LABEL), $_POST['version_id']);
 
 table_section(2);
 
-table_section_title(_("General Ledger Settings"));
-fiscalyears_list_row(_("Fiscal Year:"), 'f_year', $_POST['f_year']);
-text_row_ex(_("Tax Periods:"), 'tax_prd', 10, 10, '', null, null, _('Months.'));
-text_row_ex(_("Tax Last Period:"), 'tax_last', 10, 10, '', null, null, _('Months back.'));
-check_row(_("Put alternative Tax Include on Docs"), 'alternative_tax_include_on_docs', null);
-check_row(_("Suppress Tax Rates on Docs"), 'suppress_tax_rates', null);
-check_row(_("Automatic Revaluation Currency Accounts"), 'auto_curr_reval', $_POST['auto_curr_reval']);
+table_section_title(_(UI_TEXT_GENERAL_LEDGER_SETTINGS));
+fiscalyears_list_row(_(UI_TEXT_FISCAL_YEAR_LABEL), 'f_year', $_POST['f_year']);
+text_row_ex(_(UI_TEXT_TAX_PERIODS_LABEL), 'tax_prd', 10, 10, '', null, null, _(UI_TEXT_MONTHS));
+text_row_ex(_(UI_TEXT_TAX_LAST_PERIOD_LABEL), 'tax_last', 10, 10, '', null, null, _(UI_TEXT_MONTHS_BACK));
+check_row(_(UI_TEXT_PUT_ALTERNATIVE_TAX_INCLUDE_ON_DOCS_LABEL), 'alternative_tax_include_on_docs', null);
+check_row(_(UI_TEXT_SUPPRESS_TAX_RATES_ON_DOCS_LABEL), 'suppress_tax_rates', null);
+check_row(_(UI_TEXT_AUTOMATIC_REVALUATION_CURRENCY_ACCOUNTS_LABEL), 'auto_curr_reval', $_POST['auto_curr_reval']);
 
-table_section_title(_("Sales Pricing"));
-sales_types_list_row(_("Base for auto price calculations:"), 'base_sales', $_POST['base_sales'], false,
-    _('No base price list') );
+table_section_title(_(UI_TEXT_SALES_PRICING));
+sales_types_list_row(_(UI_TEXT_BASE_FOR_AUTO_PRICE_CALCULATIONS_LABEL), 'base_sales', $_POST['base_sales'], false,
+    _(UI_TEXT_NO_BASE_PRICE_LIST) );
 
-text_row_ex(_("Add Price from Std Cost:"), 'add_pct', 10, 10, '', null, null, "%");
+text_row_ex(_(UI_TEXT_ADD_PRICE_FROM_STD_COST_LABEL), 'add_pct', 10, 10, '', null, null, "%");
 $curr = get_currency($_POST['curr_default']);
-text_row_ex(_("Round calculated prices to nearest:"), 'round_to', 10, 10, '', null, null, $curr['hundreds_name']);
+text_row_ex(_(UI_TEXT_ROUND_CALCULATED_PRICES_LABEL), 'round_to', 10, 10, '', null, null, $curr['hundreds_name']);
 label_row("", "&nbsp;");
 
 
-table_section_title(_("Optional Modules"));
-check_row(_("Manufacturing"), 'use_manufacturing', null);
-check_row(_("Fixed Assets"), 'use_fixed_assets', null);
-number_list_row(_("Use Dimensions:"), 'use_dimension', null, 0, 2);
+table_section_title(_(UI_TEXT_OPTIONAL_MODULES));
+check_row(_(UI_TEXT_MANUFACTURING_LABEL), 'use_manufacturing', null);
+check_row(_(UI_TEXT_FIXED_ASSETS_LABEL), 'use_fixed_assets', null);
+number_list_row(_(UI_TEXT_USE_DIMENSIONS_LABEL), 'use_dimension', null, 0, 2);
 
-table_section_title(_("User Interface Options"));
+table_section_title(_(UI_TEXT_USER_INTERFACE_OPTIONS));
 
-check_row(_("Short Name and Name in List"), 'shortname_name_in_list', $_POST['shortname_name_in_list']);
-check_row(_("Open Print Dialog Direct on Reports"), 'print_dialog_direct', null);
-check_row(_("Search Item List"), 'no_item_list', null);
-check_row(_("Search Customer List"), 'no_customer_list', null);
-check_row(_("Search Supplier List"), 'no_supplier_list', null);
-text_row_ex(_("Login Timeout:"), 'login_tout', 10, 10, '', null, null, _('seconds'));
-text_row_ex(_("Max day range in documents"), 'max_days_in_docs', 10, 10, '', null, null, _('days.'));
+check_row(_(UI_TEXT_SHORT_NAME_AND_NAME_IN_LIST_LABEL), 'shortname_name_in_list', $_POST['shortname_name_in_list']);
+check_row(_(UI_TEXT_OPEN_PRINT_DIALOG_DIRECT_ON_REPORTS_LABEL), 'print_dialog_direct', null);
+check_row(_(UI_TEXT_SEARCH_ITEM_LIST_LABEL), 'no_item_list', null);
+check_row(_(UI_TEXT_SEARCH_CUSTOMER_LIST_LABEL), 'no_customer_list', null);
+check_row(_(UI_TEXT_SEARCH_SUPPLIER_LIST_LABEL), 'no_supplier_list', null);
+text_row_ex(_(UI_TEXT_LOGIN_TIMEOUT_LABEL), 'login_tout', 10, 10, '', null, null, _(UI_TEXT_SECONDS));
+text_row_ex(_(UI_TEXT_MAX_DAY_RANGE_IN_DOCUMENTS_LABEL), 'max_days_in_docs', 10, 10, '', null, null, _(UI_TEXT_DAYS));
 
 end_outer_table(1);
 
 hidden('coy_logo', $_POST['coy_logo']);
-submit_center('update', _("Update"), true, '',  'default');
+submit_center('update', _(UI_TEXT_UPDATE_BUTTON), true, '',  'default');
 
 end_form(2);
 //-------------------------------------------------------------------------------------------------
